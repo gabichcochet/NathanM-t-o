4e8b4a4ad5c211b09bdadf15b1aba0452409e192
Zephied <galmiche1105@gmail.com>
2025-11-12 15:26:27 +0100

    ajout acces wifi

--- nathemperature/src/main.cpp
+++ nathemperature/src/main.cpp
@@ -1,12 +1,34 @@
 #include <Arduino.h>
 #include "DHTesp.h"
-
+#include <WiFiManager.h>
+ 
+WiFiManager wm;
 DHTesp dht;
 
 void setup() {
+
+  WiFi.mode(WIFI_STA);
+  
   Serial.begin(115200);
+
   dht.setup(25, DHTesp::DHT11);
+
   delay(1000);
+  Serial.println();
+
+  Serial.println("Tentative de connexion au réseau Wi-Fi...");
+
+    // Connexion automatique au réseau Wi-Fi connu
+    if (!wm.autoConnect()) {
+        Serial.println("Erreur de connexion au réseau Wi-Fi.");
+        // Vous pouvez ajouter ici une logique pour gérer l'erreur de connexion
+    } else {
+        // Connexion réussie
+        Serial.println("Connexion au réseau Wi-Fi réussie !");
+        Serial.print("Adresse IP : ");
+        Serial.println(WiFi.localIP());
+        // Vous pouvez ajouter ici une logique pour exécuter des actions supplémentaires après la connexion réussie
+    }
 }
 
 void loop() {
