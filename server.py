f9f6dc1bc84575f48becfe1c9b0e5dc180c8dd08
cocgabriel <gabriel.cochet@ynov.com>
2025-12-02 10:43:30 +0100

    connexion signup

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
-import threading
 
 PORT = 8000
 
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.path = "/connexion.html"
+        return super().do_GET()
+
 def start_server():
-    server = HTTPServer(("0.0.0.0", PORT), SimpleHTTPRequestHandler)
+    server = HTTPServer(("0.0.0.0", PORT), MyHandler)
     print(f"🌐 Serving at http://localhost:{PORT}")
     server.serve_forever()
 
